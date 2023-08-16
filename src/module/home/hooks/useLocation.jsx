4b44878047185faa745e062ccd7945e8e10eb030
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2023-08-16 08:35:20 +0700

    fix: change to https

--- src/module/home/hooks/useLocation.jsx
+++ src/module/home/hooks/useLocation.jsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function useLocation(shouldFetch) {
-  const { data, error, isLoading } = useSWR(shouldFetch ? `http://api.airvisual.com/v2/nearest_city?key=${import.meta.env.VITE_API_KEY}` : null, fetcher)
+  const { data, error, isLoading } = useSWR(shouldFetch ? `https://api.airvisual.com/v2/nearest_city?key=${import.meta.env.VITE_API_KEY}` : null, fetcher)
 
   return {
     pollutan: data,
